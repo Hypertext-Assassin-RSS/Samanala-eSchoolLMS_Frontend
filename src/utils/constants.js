b6a40509dbd0833334c33210f52c268a3237317e
Hypertext-Assassin-RSS <sanjayasenanayaka11@gmail.com>
2024-05-20 11:41:24 +0530

    update base url

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,3 +1,2 @@
-// export const BASE_URL = import.meta.env.VITE_BASE_URL;
-export const BASE_URL = 'https://127.0.0.1:3000/api/';
+export const BASE_URL = import.meta.env.VITE_BASE_URL;
 export const FIREBASE_CONFIG = import.meta.env.VITE_FIREBASE_CONFIG
